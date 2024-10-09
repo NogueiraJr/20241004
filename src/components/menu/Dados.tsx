15a2c3b8bdda3af3239afdc1300715a6c83b0d24
Nogueira Jr <nogueirajr@hotmail.com>
2024-10-09 15:53:46 -0300

    Dados open Client

--- src/components/menu/Dados.tsx
+++ src/components/menu/Dados.tsx
@@ -6,49 +6,80 @@ import {
   CheckCircleOutlined, 
   CloseCircleOutlined, 
   DeleteOutlined, 
-  UserOutlined,   // Ícone para Clientes
-  ShoppingCartOutlined, // Ícone para Fornecedores
-  TeamOutlined, // Ícone para Parceiros
-  AppstoreAddOutlined // Ícone para Produtos
+  UserOutlined, 
+  ShoppingCartOutlined, 
+  TeamOutlined, 
+  AppstoreAddOutlined 
 } from '@ant-design/icons';
 import { Avatar, Card, Dropdown, Menu, Tooltip } from 'antd';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 
-const IconText = ({ icon, text, tooltip, color }: { icon: React.ComponentType<any>; text: string; tooltip: string, color?: string }) => (
+const IconText = ({ icon, text, tooltip, color, onClick }: { icon: React.ComponentType<any>; text: string; tooltip: string, color?: string, onClick?: () => void }) => (
   <Tooltip title={tooltip}>
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }} onClick={onClick}>
       {React.createElement(icon, { style: { color: color || 'black', fontSize: '20px' } })}
       <span style={{ color: color || 'black', fontSize: '12px' }}>{text}</span>
     </div>
   </Tooltip>
 );
 
-const actions = [
-  <IconText icon={PlusSquareOutlined} text="NOVO" tooltip="Adicionar Novo" key="icon-new" color="#FF4C00" />,
-  <IconText icon={UnorderedListOutlined} text="175" tooltip="Lista com Todos" key="icon-all-itens" color="blue" />,
-  <IconText icon={CheckCircleOutlined} text="123" tooltip="Apenas os Ativos" key="icon-actived-itens" color="green" />,
-  <IconText icon={CloseCircleOutlined} text="52" tooltip="Apenas os Inativos" key="icon-desatived-itens" color="gray" />,
-  <IconText icon={DeleteOutlined} text="15" tooltip="Apenas os Apagados" key="icon-deleted-itens" color="red" />,
-  <Dropdown
-    overlay={
-      <Menu>
-        <Menu.Item key="1">casamento</Menu.Item>
-        <Menu.Item key="2">batizado</Menu.Item>
-        <Menu.Item key="3">festa</Menu.Item>
-      </Menu>
-    }
-    trigger={['click']}
-  >
-    <Tooltip title="Etiquetas">
-      <EllipsisOutlined key="ellipsis" style={{ color: 'black' }} />
-    </Tooltip>
-  </Dropdown>,
-];
-
 const Dados: React.FC = () => {
+  const navigate = useNavigate();
+
+  // Função para navegar para a página de clientes ao clicar no "NOVO"
+  const handleNovoCliente = () => {
+    navigate('/cliente');
+    console.log('Toki1');
+  };
+
+  const actionsClientes = [
+    <IconText icon={PlusSquareOutlined} text="NOVO" tooltip="Adicionar Novo" key="icon-new" color="#FF4C00" onClick={handleNovoCliente} />,
+    <IconText icon={UnorderedListOutlined} text="175" tooltip="Lista com Todos" key="icon-all-itens" color="blue" />,
+    <IconText icon={CheckCircleOutlined} text="123" tooltip="Apenas os Ativos" key="icon-actived-itens" color="green" />,
+    <IconText icon={CloseCircleOutlined} text="52" tooltip="Apenas os Inativos" key="icon-desatived-itens" color="gray" />,
+    <IconText icon={DeleteOutlined} text="15" tooltip="Apenas os Apagados" key="icon-deleted-itens" color="red" />,
+    <Dropdown
+      overlay={
+        <Menu>
+          <Menu.Item key="1">casamento</Menu.Item>
+          <Menu.Item key="2">batizado</Menu.Item>
+          <Menu.Item key="3">festa</Menu.Item>
+        </Menu>
+      }
+      trigger={['click']}
+    >
+      <Tooltip title="Etiquetas">
+        <EllipsisOutlined key="ellipsis" style={{ color: 'black' }} />
+      </Tooltip>
+    </Dropdown>,
+  ];
+
+  const actions = [
+    <IconText icon={PlusSquareOutlined} text="NOVO" tooltip="Adicionar Novo" key="icon-new" color="#FF4C00" />,
+    <IconText icon={UnorderedListOutlined} text="175" tooltip="Lista com Todos" key="icon-all-itens" color="blue" />,
+    <IconText icon={CheckCircleOutlined} text="123" tooltip="Apenas os Ativos" key="icon-actived-itens" color="green" />,
+    <IconText icon={CloseCircleOutlined} text="52" tooltip="Apenas os Inativos" key="icon-desatived-itens" color="gray" />,
+    <IconText icon={DeleteOutlined} text="15" tooltip="Apenas os Apagados" key="icon-deleted-itens" color="red" />,
+    <Dropdown
+      overlay={
+        <Menu>
+          <Menu.Item key="1">casamento</Menu.Item>
+          <Menu.Item key="2">batizado</Menu.Item>
+          <Menu.Item key="3">festa</Menu.Item>
+        </Menu>
+      }
+      trigger={['click']}
+    >
+      <Tooltip title="Etiquetas">
+        <EllipsisOutlined key="ellipsis" style={{ color: 'black' }} />
+      </Tooltip>
+    </Dropdown>,
+  ];
+
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', gap: '6px' }}>
       <Card 
-        actions={actions} 
+        actions={actionsClientes} 
         style={{ 
           flex: '1 1 calc(33.333% - 5px)', 
           minWidth: 290, 
@@ -64,7 +95,7 @@ const Dados: React.FC = () => {
             <Tooltip title="Clientes">
               <Avatar icon={<UserOutlined style={{ color: 'black' }} />} />
             </Tooltip>
-          } // Ícone para Clientes
+          }
           title="Clientes"
           description={
             <>
@@ -90,7 +121,7 @@ const Dados: React.FC = () => {
             <Tooltip title="Fornecedores">
               <Avatar icon={<ShoppingCartOutlined style={{ color: 'black' }} />} />
             </Tooltip>
-          } // Ícone para Fornecedores
+          }
           title="Fornecedores"
           description={
             <>
@@ -116,7 +147,7 @@ const Dados: React.FC = () => {
             <Tooltip title="Parceiros">
               <Avatar icon={<TeamOutlined style={{ color: 'black' }} />} />
             </Tooltip>
-          } // Ícone para Parceiros
+          }
           title="Parceiros"
           description={
             <>
@@ -142,7 +173,7 @@ const Dados: React.FC = () => {
             <Tooltip title="Produtos">
               <Avatar icon={<AppstoreAddOutlined style={{ color: 'black' }} />} />
             </Tooltip>
-          } // Ícone para Produtos
+          }
           title="Produtos"
           description={
             <>
