0bcaf47d2edd0d270fda4937a348a6b4898414e9
Nogueira Jr <nogueirajr@hotmail.com>
2024-10-19 17:58:40 -0300

    Improvements in Product Selection

--- src/components/menuModule/Operacional/ReservaNovo.tsx
+++ src/components/menuModule/Operacional/ReservaNovo.tsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { Form, Input, Select, Typography } from 'antd';
-import { NumericFormat } from 'react-number-format'; // Importando NumericFormat
+import { Button, Form, Input, Select, Tag, Typography } from 'antd';
+import { NumericFormat } from 'react-number-format';
 import { produtos } from '../../fields/produtos-json';
 import { clientes } from '../../fields/clientes-json';
+import { DownOutlined, MinusOutlined, PlusOutlined, UpOutlined } from '@ant-design/icons';
 
 const { Text } = Typography;
 
@@ -11,16 +12,18 @@ const ReservaNovo: React.FC = () => {
   const [quantities, setQuantities] = useState({});
   const [selectedClient, setSelectedClient] = useState(null);
   const [editableTotalPrice, setEditableTotalPrice] = useState('0,00');
+  const [filterValue, setFilterValue] = useState(''); // Novo estado para armazenar o valor do filtro
 
   const handleProductChange = (value) => {
     setSelectedProducts(value);
     const newQuantities = {};
     value.forEach((id) => {
       if (!quantities[id]) {
-        newQuantities[id] = 1; // Default quantity is 1
+        newQuantities[id] = 1; // Quantidade padrão é 1
       }
     });
     setQuantities((prev) => ({ ...prev, ...newQuantities }));
+
     // Atualiza o totalPrice quando os produtos mudam
     const newTotalPrice = calculateTotalPrice(value, newQuantities);
     setEditableTotalPrice(newTotalPrice.toFixed(2).replace('.', ','));
@@ -67,6 +70,9 @@ const ReservaNovo: React.FC = () => {
               value: c.id,
               label: c.name,
             }))}
+            filterOption={(input, option) =>
+              option?.label.toLowerCase().includes(input.toLowerCase())
+            }
           />
         </Form.Item>
 
@@ -97,51 +103,106 @@ const ReservaNovo: React.FC = () => {
               const { formattedValue, value } = values;
               setEditableTotalPrice(formattedValue);
             }}
-            style={{ width: '96%', padding: '8px', borderRadius: '4px', border: '1px solid #d9d9d9', backgroundColor: '#fff' }} // Estilo para se assemelhar ao Input do Ant Design
-            // Estilo semelhante ao Input do Ant Design
+            style={{ width: '96%', padding: '8px', borderRadius: '4px', border: '1px solid #d9d9d9', backgroundColor: '#fff' }}
           />
         </Form.Item>
+
         <Form.Item label="Produtos">
           <Select
             mode="multiple"
             showSearch
             placeholder="Selecione os produtos"
-            options={produtos.map(p => ({
-              value: p.id,
-              label: (
-                <div style={{ display: 'flex', alignItems: 'left', flexDirection: 'column' }}>
-                  {selectedProducts.includes(p.id) && (
-                    <Input
-                      type="number"
-                      min={1}
-                      value={quantities[p.id] || 1}
-                      onChange={(e) => handleQuantityChange(p.id, parseInt(e.target.value, 10) || 1)}
-                      style={{ width: 60, marginBottom: 5 }}
-                      onMouseDown={(e) => e.stopPropagation()}
-                      onClick={(e) => e.stopPropagation()}
-                    />
-                  )}
-                  <div style={{ flexGrow: 1 }}>
-                    <Text strong>
-                      {p.productTypeId === 'product' ? 'Produto' : 'Serviço'} R$ {p.price.toFixed(2).replace('.', ',')} {p.name}
-                    </Text>
-                    <br />
-                    <Text type="secondary" style={{ fontSize: 12, whiteSpace: 'normal' }}>
-                      {p.description}
-                    </Text>
-                    <br />
-                    <Text style={{ fontSize: 12, color: p.quantity <= 0 ? 'red' : 'inherit' }}>
-                      Disponível: {p.quantity}
-                    </Text>
-                    <br />
-                    <Text style={{ fontSize: 12, color: '#1890ff' }}>
-                      {(p.tags || []).join(', ')}
-                    </Text>
+            options={produtos.map(p => {
+              const [showDetails, setShowDetails] = useState(false);
+
+              return {
+                value: p.id,
+                name: p.name,
+                label: (
+                  <div style={{
+                    display: 'flex',
+                    alignItems: 'left',
+                    flexDirection: 'column',
+                    borderLeft: '1px solid black',
+                    paddingLeft: 10,
+                    marginRight: 2
+                  }}>
+                    <div style={{ flexGrow: 1 }}>
+                      <Text strong>
+                        {p.productTypeId === 'product' ? 'Produto' : 'Serviço'} R$ {p.price.toFixed(2).replace('.', ',')} {p.name}
+                      </Text>
+                      <br />
+                      <div style={{ display: 'flex', alignItems: 'center', marginTop: 5 }}>
+                        <span style={{ marginRight: 10 }}>
+                          Disponível: {(p.quantity).toString().padStart(3, '0')}
+                        </span>
+                        <Button
+                          type="primary"
+                          size="small"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            const newQuantity = (quantities[p.id] || 1) - 1;
+                            if (newQuantity >= 1) {
+                              handleQuantityChange(p.id, newQuantity);
+                            }
+                          }}
+                          disabled={(quantities[p.id] || 1) <= 1}
+                        >
+                          <MinusOutlined />
+                        </Button>
+                        <span style={{ margin: '0 10px' }}>
+                          {(quantities[p.id] || 1).toString().padStart(3, '0')}
+                        </span>
+                        <Button
+                          type="primary"
+                          size="small"
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            const newQuantity = (quantities[p.id] || 1) + 1;
+                            if (newQuantity <= p.quantity) {
+                              handleQuantityChange(p.id, newQuantity);
+                            }
+                          }}
+                          disabled={(quantities[p.id] || 1) >= p.quantity}
+                        >
+                          <PlusOutlined />
+                        </Button>
+                        <Button
+                          type="primary"
+                          size="small"
+                          style={{ marginLeft: 10 }}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setShowDetails(!showDetails);
+                          }}
+                        >
+                          {showDetails ? <UpOutlined /> : <DownOutlined />}
+                        </Button>
+                      </div>
+                      {showDetails && (
+                        <>
+                          <Text type="secondary" style={{ fontSize: 12, whiteSpace: 'normal', marginTop: 0 }}>
+                            {p.description}
+                          </Text>
+                          <br />
+                          <div style={{ marginTop: 5 }}>
+                            {(p.tags || []).map((tag, index) => (
+                              <Tag key={index} color="blue" style={{ margin: '2px' }}>
+                                {tag}
+                              </Tag>
+                            ))}
+                          </div>
+                        </>
+                      )}
+                    </div>
                   </div>
-                </div>
-              ),
-            }))}
+                ),
+              };
+            })}
             onChange={handleProductChange}
+            filterOption={(input, option) =>
+              option?.name.toLowerCase().includes(input.toLowerCase())
+            }
           />
         </Form.Item>
       </Form>
