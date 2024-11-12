8dafb2378c59065769ba023eac9f70e4df6aa92b
Nogueira Jr <nogueirajr@hotmail.com>
2024-11-12 16:43:12 -0300

    Filter in Cliente and Produto

--- src/components/menuModule/Dados/Cliente.tsx
+++ src/components/menuModule/Dados/Cliente.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Space, Table, Tag, Tooltip } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Space, Table, Tag, Tooltip, Input, Select } from 'antd';
 import type { TableProps } from 'antd';
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import { useParameter } from '../../../context/ParameterContext';
@@ -23,101 +23,161 @@ const IconText = ({ icon, text, tooltip, color, onClick }: { icon: React.Compone
   </Tooltip>
 );
 
-const columns: TableProps<ClientType>['columns'] = [
-  {
-    title: 'Nome',
-    dataIndex: 'name',
-    key: 'name',
-    width: '100%', // ajuste conforme necessário
-  },
-  {
-    title: 'Ações',
-    key: 'action',
-    width: '100px',
-    render: (_, record) => (
-      <Space size="middle">
-        <IconText icon={EditOutlined} text="EDITAR" tooltip="Editar o Item" key="icon-edit" color="black" />
-        <IconText icon={DeleteOutlined} text="APAGAR" tooltip="Apagar o Item" key="icon-delete" color="black" />
-      </Space>
-    ),
-  },
-];
-
 const Cliente: React.FC = () => {
   let clientes = [];
   const { system } = useParameter();
 
   const [expandedRowKeys, setExpandedRowKeys] = useState<string[]>([]);
+  const [searchText, setSearchText] = useState('');
+  const [statusFilter, setStatusFilter] = useState<string>('all');
+  const [tagFilter, setTagFilter] = useState<string>('all');
+  const [filteredData, setFilteredData] = useState<ClientType[]>([]);
 
   const handleExpand = (expanded: boolean, record: ClientType) => {
     setExpandedRowKeys((prevExpandedRowKeys) => {
-      if (expanded) {
-        // Adiciona a chave do item expandido à lista
-        return [...prevExpandedRowKeys, record.id];
-      } else {
-        // Remove a chave do item contraído da lista
-        return prevExpandedRowKeys.filter((key) => key !== record.id);
-      }
+      return expanded ? [...prevExpandedRowKeys, record.id] : prevExpandedRowKeys.filter((key) => key !== record.id);
     });
   };
 
+  const handleSearch = (value: string) => {
+    setSearchText(value);
+  };
+
+  const handleStatusFilter = (value: string) => {
+    setStatusFilter(value);
+  };
+
+  const handleTagFilter = (value: string) => {
+    setTagFilter(value);
+  };
+
+  // Ajustar os dados de clientes conforme o sistema
   switch (system) {
     case 'sysLocacaoRoupa':
-      clientes = clientesLocacaoRoupa;	
+      clientes = clientesLocacaoRoupa;
       break;
-  
     case 'sysOficinaCarros':
-      clientes = clientesOficinaCarros;	
+      clientes = clientesOficinaCarros;
       break;
-      
     default:
       break;
-    }
-  
+  }
+
+  // Atualizar filteredData sempre que searchText, statusFilter, tagFilter ou clientes mudarem
+  useEffect(() => {
+    const filtered = clientes.filter((cliente) => {
+      const matchesName = cliente.name.toLowerCase().includes(searchText.toLowerCase());
+      const matchesStatus = statusFilter === 'all' || cliente.active === (statusFilter === 'active');
+      const matchesTag = tagFilter === 'all' || (cliente.tags && cliente.tags.includes(tagFilter));
+
+      // Apenas exibe os itens que correspondem a todos os filtros aplicados
+      return matchesName && matchesStatus && matchesTag;
+    });
+    setFilteredData(filtered);
+  }, [searchText, statusFilter, tagFilter, clientes]);
+
+  const columns: TableProps<ClientType>['columns'] = [
+    {
+      title: 'Nome',
+      dataIndex: 'name',
+      key: 'name',
+      width: '100%',
+      filterDropdown: () => (
+        <div style={{ padding: 8 }}>
+          <Input
+            placeholder="Pesquisar por nome"
+            value={searchText}
+            onChange={(e) => handleSearch(e.target.value)}
+            style={{ marginBottom: 8, display: 'block' }}
+          />
+        </div>
+      ),
+    },
+    {
+      title: 'Ações',
+      key: 'action',
+      width: '100px',
+      render: (_, record) => (
+        <Space size="middle">
+          <IconText icon={EditOutlined} text="EDITAR" tooltip="Editar o Item" key="icon-edit" color="black" />
+          <IconText icon={DeleteOutlined} text="APAGAR" tooltip="Apagar o Item" key="icon-delete" color="black" />
+        </Space>
+      ),
+    },
+  ];
+
   return (
-    <Table<ClientType>
-      columns={columns}
-      dataSource={clientes}
-      pagination={{ position: ['topLeft'] }}
-      expandedRowRender={(record) => {
-        const formattedDate = new Date(record.createAt).toLocaleString('pt-BR', {
-          day: '2-digit',
-          month: '2-digit',
-          year: 'numeric',
-          hour: '2-digit',
-          minute: '2-digit',
-        });
+    <>
+      <div style={{ marginBottom: 16 }}>
+        <Select
+          placeholder="Filtrar por status"
+          onChange={handleStatusFilter}
+          style={{ width: 150, marginRight: 8 }}
+          defaultValue="all"
+        >
+          <Select.Option value="all">Todos</Select.Option>
+          <Select.Option value="active">Ativo</Select.Option>
+          <Select.Option value="inactive">Inativo</Select.Option>
+        </Select>
 
-        const tagsDisplay = record.tags && record.tags.length > 0
-          ? record.tags.map((tag) => {
-            let color = tag.length > 5 ? 'geekblue' : 'green';
-            return (
-              <Tag color={color} key={tag}>
-                {tag.toUpperCase()}
-              </Tag>
-            );
-          })
-          : null;
+        <Select
+          placeholder="Filtrar por tag"
+          onChange={handleTagFilter}
+          style={{ width: 150 }}
+          defaultValue="all"
+        >
+          <Select.Option value="all">Todos</Select.Option>
+          {Array.from(new Set(clientes.flatMap(cliente => cliente.tags || []))).map(tag => (
+            <Select.Option key={tag} value={tag}>
+              {tag.toUpperCase()}
+            </Select.Option>
+          ))}
+        </Select>
+      </div>
 
-        return (
-          <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', color: 'gray' }}>
-            <span style={{ marginRight: '8px' }}>Criado em {formattedDate}</span> |
-            <Tooltip title={'Situação do item'}>
-              <span style={{ cursor: 'pointer', marginLeft: '8px', color: record.active ? 'green' : 'red' }}> {record.active ? 'ATIVO' : 'INATIVO'} </span>
-            </Tooltip>
-            {tagsDisplay && tagsDisplay.length > 0 && (
-              <span style={{ cursor: 'pointer', marginLeft: '8px' }}>
-                | {tagsDisplay}
-              </span>
-            )}
-          </div>
-        );
-      }}
-      expandedRowKeys={expandedRowKeys}
-      onExpand={handleExpand}
-      rowKey="id"
-    />
-  )
+      <Table<ClientType>
+        columns={columns}
+        dataSource={filteredData}
+        pagination={{ position: ['topLeft'] }}
+        expandedRowRender={(record) => {
+          const formattedDate = new Date(record.createAt).toLocaleString('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          });
+
+          const tagsDisplay = record.tags && record.tags.length > 0
+            ? record.tags.map((tag) => (
+                <Tag color={tag.length > 5 ? 'geekblue' : 'green'} key={tag}>
+                  {tag.toUpperCase()}
+                </Tag>
+              ))
+            : null;
+
+          return (
+            <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', color: 'gray' }}>
+              <span style={{ marginRight: '8px' }}>Criado em {formattedDate}</span> |
+              <Tooltip title={'Situação do item'}>
+                <span style={{ cursor: 'pointer', marginLeft: '8px', color: record.active ? 'green' : 'red' }}>
+                  {record.active ? 'ATIVO' : 'INATIVO'}
+                </span>
+              </Tooltip>
+              {tagsDisplay && tagsDisplay.length > 0 && (
+                <span style={{ cursor: 'pointer', marginLeft: '8px' }}>
+                  | {tagsDisplay}
+                </span>
+              )}
+            </div>
+          );
+        }}
+        expandedRowKeys={expandedRowKeys}
+        onExpand={handleExpand}
+        rowKey="id"
+      />
+    </>
+  );
 };
 
 export default Cliente;
--- src/components/menuModule/Dados/Produto.tsx
+++ src/components/menuModule/Dados/Produto.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Space, Table, Tag, Tooltip } from 'antd';
+import React, { useState, useEffect } from 'react';
+import { Space, Table, Tag, Tooltip, Input, Select } from 'antd';
 import type { TableProps } from 'antd';
 import { DeleteOutlined, EditOutlined } from '@ant-design/icons';
 import { produtosLocacaoRoupa } from '../../fields/produtosLocacaoRoupa-json';
@@ -30,105 +30,162 @@ const IconText = ({ icon, text, tooltip, color, onClick }: { icon: React.Compone
   </Tooltip>
 );
 
-const columns: TableProps<ProductType>['columns'] = [
-  {
-    title: 'Nome',
-    dataIndex: 'name',
-    key: 'name',
-    width: '100%',
-  },
-  {
-    title: 'Ações',
-    key: 'action',
-    width: '100px',
-    render: (_, record) => (
-      <Space size="middle">
-        <IconText icon={EditOutlined} text="EDITAR" tooltip="Editar o Item" key="icon-edit" color="black" />
-        <IconText icon={DeleteOutlined} text="APAGAR" tooltip="Apagar o Item" key="icon-delete" color="black" />
-      </Space>
-    ),
-  },
-];
-
 const Produto: React.FC = () => {
   let produtos = [];
   const { system } = useParameter();
 
   const [expandedRowKeys, setExpandedRowKeys] = useState<string[]>([]);
+  const [searchText, setSearchText] = useState('');
+  const [statusFilter, setStatusFilter] = useState<string>('all');
+  const [tagFilter, setTagFilter] = useState<string>('all');
+  const [filteredData, setFilteredData] = useState<ProductType[]>([]);
 
   const handleExpand = (expanded: boolean, record: ProductType) => {
     setExpandedRowKeys((prevExpandedRowKeys) => {
-      if (expanded) {
-        // Adiciona a chave do item expandido à lista
-        return [...prevExpandedRowKeys, record.id];
-      } else {
-        // Remove a chave do item contraído da lista
-        return prevExpandedRowKeys.filter((key) => key !== record.id);
-      }
+      return expanded ? [...prevExpandedRowKeys, record.id] : prevExpandedRowKeys.filter((key) => key !== record.id);
     });
   };
 
-  switch (system) {
-	case 'sysLocacaoRoupa':
-		produtos = produtosLocacaoRoupa;	
-		break;
+  const handleSearch = (value: string) => {
+    setSearchText(value);
+  };
 
-	case 'sysOficinaCarros':
-		produtos = produtosOficinaCarros;	
-		break;
-	  
-	default:
-		break;
+  const handleStatusFilter = (value: string) => {
+    setStatusFilter(value);
+  };
+
+  const handleTagFilter = (value: string) => {
+    setTagFilter(value);
+  };
+
+  // Ajustar os dados de produtos conforme o sistema
+  switch (system) {
+    case 'sysLocacaoRoupa':
+      produtos = produtosLocacaoRoupa;
+      break;
+    case 'sysOficinaCarros':
+      produtos = produtosOficinaCarros;
+      break;
+    default:
+      break;
   }
 
-  return (
-    <Table<ProductType>
-      columns={columns}
-      dataSource={produtos}
-      pagination={{ position: ['topLeft'] }}
-      expandedRowRender={(record) => {
-        const formattedDate = new Date(record.createAt).toLocaleString('pt-BR', {
-          day: '2-digit',
-          month: '2-digit',
-          year: 'numeric',
-          hour: '2-digit',
-          minute: '2-digit',
-        });
+  // Atualizar filteredData sempre que searchText, statusFilter, tagFilter ou produtos mudarem
+  useEffect(() => {
+    const filtered = produtos.filter((produto) => {
+      const matchesName = produto.name.toLowerCase().includes(searchText.toLowerCase());
+      const matchesStatus = statusFilter === 'all' || produto.active === (statusFilter === 'active');
+      const matchesTag = tagFilter === 'all' || (produto.tags && produto.tags.includes(tagFilter));
+      return matchesName && matchesStatus && matchesTag;
+    });
 
-        const tagsDisplay = record.tags && record.tags.length > 0 
-          ? record.tags.map((tag) => {
-              let color = tag.length > 5 ? 'geekblue' : 'green';
-              return (
-                <Tag color={color} key={tag}>
+    setFilteredData(filtered);
+  }, [searchText, statusFilter, tagFilter, produtos]);
+
+  const columns: TableProps<ProductType>['columns'] = [
+    {
+      title: 'Nome',
+      dataIndex: 'name',
+      key: 'name',
+      width: '100%',
+      filterDropdown: () => (
+        <div style={{ padding: 8 }}>
+          <Input
+            placeholder="Pesquisar por nome"
+            value={searchText}
+            onChange={(e) => handleSearch(e.target.value)}
+            style={{ marginBottom: 8, display: 'block' }}
+          />
+        </div>
+      ),
+    },
+    {
+      title: 'Ações',
+      key: 'action',
+      width: '100px',
+      render: (_, record) => (
+        <Space size="middle">
+          <IconText icon={EditOutlined} text="EDITAR" tooltip="Editar o Item" key="icon-edit" color="black" />
+          <IconText icon={DeleteOutlined} text="APAGAR" tooltip="Apagar o Item" key="icon-delete" color="black" />
+        </Space>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <div style={{ marginBottom: 16 }}>
+        <Select
+          placeholder="Filtrar por status"
+          onChange={handleStatusFilter}
+          style={{ width: 150, marginRight: 8 }}
+          defaultValue="all"
+        >
+          <Select.Option value="all">Todos</Select.Option>
+          <Select.Option value="active">Ativo</Select.Option>
+          <Select.Option value="inactive">Inativo</Select.Option>
+        </Select>
+
+        <Select
+          placeholder="Filtrar por tag"
+          onChange={handleTagFilter}
+          style={{ width: 150 }}
+          defaultValue="all"
+        >
+          <Select.Option value="all">Todos</Select.Option>
+          {Array.from(new Set(produtos.flatMap(produto => produto.tags || []))).map(tag => (
+            <Select.Option key={tag} value={tag}>
+              {tag.toUpperCase()}
+            </Select.Option>
+          ))}
+        </Select>
+      </div>
+
+      <Table<ProductType>
+        columns={columns}
+        dataSource={filteredData}
+        pagination={{ position: ['topLeft'] }}
+        expandedRowRender={(record) => {
+          const formattedDate = new Date(record.createAt).toLocaleString('pt-BR', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+          });
+
+          const tagsDisplay = record.tags && record.tags.length > 0
+            ? record.tags.map((tag) => (
+                <Tag color={tag.length > 5 ? 'geekblue' : 'green'} key={tag}>
                   {tag.toUpperCase()}
                 </Tag>
-              );
-            })
-          : null;
+              ))
+            : null;
 
-        return (
-          <div style={{ color: 'gray' }}>
-            <div>{record.description}</div>
-            <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
-              <span style={{ marginRight: '8px' }}>Criado em {formattedDate}</span> |
-              <Tooltip title={'Situação do item'}>
-                <span style={{ cursor: 'pointer', marginLeft: '8px', color: record.active ? 'green' : 'red' }}>
-                  {record.active ? 'ATIVO' : 'INATIVO'}
-                </span>
-              </Tooltip>
-              {tagsDisplay && tagsDisplay.length > 0 && (
-                <span style={{ cursor: 'pointer', marginLeft: '8px' }}>
-                  | {tagsDisplay}
-                </span>
-              )}
+          return (
+            <div style={{ color: 'gray' }}>
+              <div>{record.description}</div>
+              <div style={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap' }}>
+                <span style={{ marginRight: '8px' }}>Criado em {formattedDate}</span> |
+                <Tooltip title={'Situação do item'}>
+                  <span style={{ cursor: 'pointer', marginLeft: '8px', color: record.active ? 'green' : 'red' }}>
+                    {record.active ? 'ATIVO' : 'INATIVO'}
+                  </span>
+                </Tooltip>
+                {tagsDisplay && tagsDisplay.length > 0 && (
+                  <span style={{ cursor: 'pointer', marginLeft: '8px' }}>
+                    | {tagsDisplay}
+                  </span>
+                )}
+              </div>
             </div>
-          </div>
-        );
-      }}
-      expandedRowKeys={expandedRowKeys}
-      onExpand={handleExpand}
-      rowKey="id"
-    />
+          );
+        }}
+        expandedRowKeys={expandedRowKeys}
+        onExpand={handleExpand}
+        rowKey="id"
+      />
+    </>
   );
 };
 
