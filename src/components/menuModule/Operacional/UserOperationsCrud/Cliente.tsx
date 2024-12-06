4fb1b6bceeeef7f7fefbd769b6b0010c70c68c84
Nogueira Jr <nogueirajr@hotmail.com>
2024-12-06 14:31:29 -0300

    Register New Customer Button

--- src/components/menuModule/Operacional/UserOperationsCrud/Cliente.tsx
+++ src/components/menuModule/Operacional/UserOperationsCrud/Cliente.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Form, Select, Button, Tooltip } from 'antd';
+import { PlusOutlined } from '@ant-design/icons'; // Importa o ícone desejado
 import '../../../../index.css';
 
 const Cliente: React.FC<{ handleClienteChange: (value: string) => void; clientes: any[] }> = ({ handleClienteChange, clientes }) => (
-  <>
-    <Form.Item
-      label={<span style={{ whiteSpace: 'nowrap' }} className="custom-label">Cliente</span>}
-      name="cliente"
-      rules={[{ required: true, message: 'Por favor, selecione um cliente!' }]}
-    >
+  <Form.Item
+    label={<span style={{ whiteSpace: 'nowrap' }} className="custom-label">Cliente</span>}
+    name="cliente"
+    rules={[{ required: true, message: 'Por favor, selecione um cliente!' }]}
+  >
+    <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}> {/* Container flex para alinhamento */}
       <Select
         showSearch
         className="custom-field"
@@ -18,14 +19,17 @@ const Cliente: React.FC<{ handleClienteChange: (value: string) => void; clientes
         filterOption={(input, option) =>
           option?.label.toLowerCase().includes(input.toLowerCase())
         }
+        style={{ flex: 1 }} // Faz o Select ocupar o máximo de espaço disponível
       />
-    </Form.Item>
-    <Form.Item>
-      <Tooltip title="Caso o Cliente ainda não exista cadastrado">
-        <Button type="primary" className="custom-button">Novo Cliente</Button>
+      <Tooltip title="Cadastrar Cliente, caso ele ainda não exista">
+        <Button
+          type="primary" // Mantém a cor original
+          icon={<PlusOutlined />} // Apenas o ícone
+          className="custom-button"
+        />
       </Tooltip>
-    </Form.Item>
-  </>
+    </div>
+  </Form.Item>
 );
 
 export default Cliente;
