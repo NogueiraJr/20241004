75c0ee37e401ef7d2723886837eb362f49957500
Nogueira Jr <nogueirajr@hotmail.com>
2024-12-20 14:45:05 -0300

    PWA part 01

---
+++ public/serviceWorker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "ocst-app-cache-v1";
+const urlsToCache = ["/", "/ini", "/dds", "/opr", "/utl"]; // Rotas a serem armazenadas em cache.
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
