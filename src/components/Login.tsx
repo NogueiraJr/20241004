57f16287388c51e3b8cca748fe7efb0110f58d2f
Nogueira Jr <nogueirajr@hotmail.com>
2024-12-22 19:01:37 -0300

    Login part 08 logo

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { Button, Form, Input, Typography, message } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { useParameter } from '../context/ParameterContext';
+import '../index.css'; // Importa o arquivo de estilo
+import logo from '../assets/images/logo.png';
 
 const { Title } = Typography;
 
@@ -15,10 +17,10 @@ const Login: React.FC = () => {
 
     setLoading(true);
 
-    if (username === 'oficina' && password === 'oficina') {
+    if (username.toLowerCase() === 'oficina' && password === '123') {
       setParameter('sysOficinaCarro'); // Define 'system' no contexto
       navigate('/ini'); // Redireciona para a página inicial
-    } else if (username === 'locacaoroupa' && password === 'locacaoroupa') {
+    } else if (username.toLowerCase() === 'locacao' && password === '123') {
       setParameter('sysLocacaoRoupa'); // Define 'system' no contexto
       navigate('/ini'); // Redireciona para a página inicial
     } else {
@@ -29,10 +31,15 @@ const Login: React.FC = () => {
   };
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <div style={{ width: '300px', padding: '20px', boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)', borderRadius: '10px' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', flexDirection: 'column' }}>
+      <img 
+        src={logo} 
+        alt="OCST App" 
+        style={{ borderRadius: '10px' }}
+      />
+      <div style={{ width: '300px', borderRadius: '10px' }}>
         <Title level={3} style={{ textAlign: 'center' }}>
-          Login
+          OCST App
         </Title>
         <Form
           name="login"
@@ -41,23 +48,23 @@ const Login: React.FC = () => {
           initialValues={{ username: '', password: '' }}
         >
           <Form.Item
-            label="Usuário"
+            label={<span className="custom-label">Usuário</span>}
             name="username"
             rules={[{ required: true, message: 'Por favor, insira seu usuário!' }]}
           >
-            <Input placeholder="Usuário" />
+            <Input className="custom-field" placeholder="Usuário" />
           </Form.Item>
 
           <Form.Item
-            label="Senha"
+            label={<span className="custom-label">Senha</span>}
             name="password"
             rules={[{ required: true, message: 'Por favor, insira sua senha!' }]}
           >
-            <Input.Password placeholder="Senha" />
+            <Input.Password className="custom-field" placeholder="Senha" />
           </Form.Item>
 
           <Form.Item>
-            <Button type="primary" htmlType="submit" block loading={loading}>
+            <Button type="primary" htmlType="submit" className="custom-button" loading={loading}>
               Entrar
             </Button>
           </Form.Item>
