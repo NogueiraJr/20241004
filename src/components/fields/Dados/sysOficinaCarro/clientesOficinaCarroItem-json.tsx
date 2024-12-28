c28d557c2da1ff8e2e0a87d7f90d58960c9252e9
Nogueira Jr <nogueirajr@hotmail.com>
2024-12-28 20:08:55 -0300

    Client Item 05 - Json Client Items

---
+++ src/components/fields/Dados/sysOficinaCarro/clientesOficinaCarroItem-json.tsx
@@ -0,0 +1,66 @@
+export const clientesOficinaCarroItem = [
+  {
+    "id": "v1",
+    "userClientId": "cm1wfe41d0001p8795aihoxtu",
+    "name": "Toyota Corolla",
+    "description": "",
+    "active": true,
+    "createAt": "2024-10-05T20:26:02.738Z",
+    "updatedAt": "2024-10-05T20:26:02.738Z",
+    "deletedAt": null
+  },
+  {
+    "id": "v2",
+    "userClientId": "cm1wfe41d0001p8795aihoxtu",
+    "name": "Honda Civic",
+    "description": "",
+    "active": true,
+    "createAt": "2024-10-05T20:26:02.738Z",
+    "updatedAt": "2024-10-05T20:26:02.738Z",
+    "deletedAt": null
+  },
+
+  {
+    "id": "v3",
+    "userClientId": "cm1wfe41o0004p879vczh6fkt",
+    "name": "Ford Focus",
+    "description": "",
+    "active": true,
+    "createAt": "2024-10-05T20:26:02.738Z",
+    "updatedAt": "2024-10-05T20:26:02.738Z",
+    "deletedAt": null
+  },
+
+  {
+    "id": "v4",
+    "userClientId": "cm1wfe41o0004p879vczh6fkt",
+    "name": "Fiat Uno",
+    "description": "",
+    "active": true,
+    "createAt": "2024-10-05T20:26:02.738Z",
+    "updatedAt": "2024-10-05T20:26:02.738Z",
+    "deletedAt": null
+  },
+
+  {
+    "id": "v5",
+    "userClientId": "cm1wfe41r0005p8796jfwz3dg",
+    "name": "Fusca",
+    "description": "",
+    "active": true,
+    "createAt": "2024-10-05T20:26:02.738Z",
+    "updatedAt": "2024-10-05T20:26:02.738Z",
+    "deletedAt": null
+  },
+
+  {
+    "id": "v6",
+    "userClientId": "cm1wfe41x0007p879q05ei1zx",
+    "name": "Fiat Palio",
+    "description": "",
+    "active": true,
+    "createAt": "2024-10-05T20:26:02.738Z",
+    "updatedAt": "2024-10-05T20:26:02.738Z",
+    "deletedAt": null
+  },
+];
