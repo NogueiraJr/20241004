b1d312fc7a806c4a2619b189f11f65b5ddd9c48b
Nogueira Jr <nogueirajr@hotmail.com>
2024-12-29 08:43:55 -0300

    Part 01 - Get System

--- src/components/menu/Inicial.tsx
+++ src/components/menu/Inicial.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { useParameter } from '../../context/ParameterContext';
 
 const Inicial: React.FC = () => {
+  const { system } = useParameter();
   return <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', fontSize: '18px', fontFamily: "sans-serif" }}>Inicial (em breve) <p> Use o menu do canto superior esquerdo </p></div>;
 };
 
