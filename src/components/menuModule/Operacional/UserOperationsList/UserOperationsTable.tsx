92408fb8131030f692d3b009fc6e278ef803dc07
Nogueira Jr <nogueirajr@hotmail.com>
2025-01-12 18:07:57 -0300

    Phase 20 - Get When Operation Function

--- src/components/menuModule/Operacional/UserOperationsList/UserOperationsTable.tsx
+++ src/components/menuModule/Operacional/UserOperationsList/UserOperationsTable.tsx
@@ -13,6 +13,7 @@ import DefaultActionMap from "./functions/defaultActionMap";
 import getColumnsForTab from "./functions/getColumnsForTab";
 import showConfirm from "./functions/showConfirm";
 import tableOperations from "./functions/tableOperations";
+import getWhenOperation from "./functions/getWhenOperation";
 
 const { Panel } = Collapse;
 
@@ -137,44 +138,7 @@ const OperationsTable: React.FC<{
                 tags: userAction.tags,
                 notes: userAction.notes,
                 scheduledAt: (
-                  <Popover
-                    content={
-                      <Steps direction="vertical" size="small">
-                        <Steps.Step
-                          title="Agendamento"
-                          description={moment(userAction.scheduledAt).format("DD/MM/YYYY HH:mm")}
-                          status={getStepStatus(userAction.scheduledAt)}
-                          icon={<CalendarOutlined style={{ color: getStepIconColor(userAction.scheduledAt, 'blue') }} />}
-                        />
-                        {userAction.executedAt && (
-                          <Steps.Step
-                            title="Execução"
-                            description={moment(userAction.executedAt).format("DD/MM/YYYY HH:mm")}
-                            status={getStepStatus(userAction.executedAt)}
-                            icon={<CalendarOutlined style={{ color: getStepIconColor(userAction.executedAt, 'red') }} />}
-                          />
-                        )}
-                        {userAction.finishedAt && (
-                          <Steps.Step
-                            title="Finalização"
-                            description={moment(userAction.finishedAt).format("DD/MM/YYYY HH:mm")}
-                            status={getStepStatus(userAction.finishedAt)}
-                            icon={<CalendarOutlined style={{ color: getStepIconColor(userAction.finishedAt, 'green') }} />}
-                          />
-                        )}
-                      </Steps>
-                    }
-                    title="Quando"
-                    trigger="click"
-                    placement="bottom"
-                  >
-                    <Tooltip title="Passos da Ação">
-                      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', cursor: 'pointer' }}>
-                        <CalendarOutlined style={{ color: lastDateColor }} />
-                        <div style={{ color: lastDateColor }}>Quando</div>
-                      </div>
-                    </Tooltip>
-                  </Popover>
+                  getWhenOperation(userAction, getStepStatus, getStepIconColor, lastDateColor)
                 ),
                 action: userAction.actionId,
                 executedAt: userAction.executedAt,
@@ -264,15 +228,15 @@ const OperationsTable: React.FC<{
     return (
       <>
         {FilterTop({ navigate, setStatusFilter, setTagFilter, operations })}
-
         {tableOperations(columns, filteredData, renderTabs, action, expandedRowKeys, handleExpand)}
-
         {ModalItens(modalTitle, isModalOpen, closeModal, modalData, getColorForTag, columns)}
       </>
     );
   };
 
 export default OperationsTable;
+
+
 
 
 
